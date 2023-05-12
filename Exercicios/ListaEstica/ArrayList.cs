34df95a3e9b726dd22e944a337f1c4627316ff88
Levi <lnatamm25@gmail.com>
2023-05-12 16:42:17 -0300

    Método "add(T data)" implementado

--- Exercicios/ListaEstica/ArrayList.cs
+++ Exercicios/ListaEstica/ArrayList.cs
@@ -21,5 +21,33 @@ namespace C_.Exercicios.ListaEstica
 
         }
 
+        private void createEspace()
+        {
+
+            T[] aux = array;
+
+            array = new T[array.Length + array.Length / 2];
+
+            for(int i = 0; i < aux.Length; i++)
+            {
+                array[i] = aux[i];
+            }
+
+        }
+
+        public void add(T data)
+        {
+            try
+            {
+                array[count] = data;
+                count++;
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                createEspace();
+                add(data);
+            }
+        }
+
     }
 }
