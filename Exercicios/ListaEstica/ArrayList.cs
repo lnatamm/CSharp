1e699fbf3b49cfc562fff615c0b18c3c0f597d64
Levi <lnatamm25@gmail.com>
2023-05-12 16:29:31 -0300

    Criando classe "ArrayList" (Lista Estática)

---
+++ Exercicios/ListaEstica/ArrayList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Exercicios.ListaEstica
+{
+    internal class ArrayList<T>
+    {
+
+        private T[] array;
+
+        private int count;
+
+        public ArrayList()
+        {
+
+            array = new T[10];
+            count = 0;
+
+        }
+
+    }
+}
