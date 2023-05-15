a057c9a415485c9b6e57bfb859c7acd00c834df4
Levi <lnatamm25@gmail.com>
2023-05-15 20:53:28 -0300

    Adicionado os getters e setters

--- Exercicios/ListaDinamica/Node.cs
+++ Exercicios/ListaDinamica/Node.cs
@@ -11,11 +11,32 @@ namespace C_.Exercicios.ListaDinamica
 
         private T data { get; set; }
 
-        private Node<T> next { get; set; }
+        private Node<T> next;
 
         public Node(T data)
         {
             this.data = data;
+            next = null;
+        }
+
+        public T getData()
+        {
+            return data;
+        }
+
+        public Node<T> getNext()
+        {
+            return next;
+        }
+
+        public void setData(T data)
+        {
+            this.data = data;
+        }
+
+        public void setNext(Node<T> next)
+        {
+            this.next = next;
         }
 
     }
