5ddefc6143d9531d4334c58f3e4452b1e80b8728
Levi <lnatamm25@gmail.com>
2023-05-15 20:37:31 -0300

    Classe "LinkedList" criada em "ListaDinamica"com o método "Count()"

---
+++ Exercicios/ListaDinamica/LinkedList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Exercicios.ListaDinamica
+{
+    internal class LinkedList<T>
+    {
+
+        private Node<T> head;
+        private Node<T> tail;
+        private int count;
+        public LinkedList() 
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+    }
+}
