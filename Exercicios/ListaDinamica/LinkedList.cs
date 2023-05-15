b3b634263771ff0c7f8d2c1b4a1910cc1dd0565e
Levi <lnatamm25@gmail.com>
2023-05-15 20:52:17 -0300

    Método "IsEmpty()", "Add(T elemento)" e "Add(int posicao, T elemento)" implementados

--- Exercicios/ListaDinamica/LinkedList.cs
+++ Exercicios/ListaDinamica/LinkedList.cs
@@ -24,5 +24,81 @@ namespace C_.Exercicios.ListaDinamica
             return count;
         }
 
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if(head == null)
+            {
+                head = node;
+                tail = node;
+                count++;
+            }
+            else
+            {
+                tail.setNext(node);
+                tail = node;
+                count++;
+            }
+        }
+
+        public void add(int i,  T data)
+        {
+
+            Node<T> node = new Node<T>(data);
+            Node<T> aux = head;
+
+            if (i == 0)
+            {
+                if(head == null)
+                {
+                    head = node;
+                    tail = node;
+                    count++;
+                }
+                else
+                {
+                    tail.setNext(node);
+                    tail = node;
+                }
+            }
+
+            else if(i == count)
+            {
+                tail.setNext(node);
+                tail = node;
+            }
+
+            else
+            {
+                if(i > count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    for(int j = 0; j < i - 1; j++)
+                    {
+                        aux = aux.getNext();
+                    }
+                    try
+                    {
+                        node.setNext(aux.getNext());
+                        aux.setNext(node);
+                        count++;
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+
+        }
+
     }
 }
