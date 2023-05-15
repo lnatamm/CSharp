21ed5de4b65e5588958c5733fce22fab11a7098d
Levi <lnatamm25@gmail.com>
2023-05-15 20:33:44 -0300

    Classe "Node" em "ListaDinamica" criada

---
+++ Exercicios/ListaDinamica/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_.Exercicios.ListaDinamica
+{
+    internal class Node<T>
+    {
+
+        private T data { get; set; }
+
+        private Node<T> next { get; set; }
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+
+    }
+}
